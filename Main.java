c8857a67f2b3af3d9b2a5a68e5035e8275f997c6
Виктор Руменов Киров <VRKirov19@codingburgas.bg>
2023-02-14 09:17:12 +0200

    14/2/2023

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args){
-        Map<String, Map<String, Integer>> hashMap = new HashMap<>();
-        hashMap.put("123", new HashMap<>());
-        hashMap.get("123").put("Ivan",20);
-        hashMap.put ("456", new HashMap<>());
-        hashMap.get("456").put("Gosho", 30);
-
-        for(String key : hashMap.keySet()){
-            String mainKey = key;
-            Map<String, Integer> innerMap = hashMap.get(key);
-            for(String innerKey : innerMap.keySet()){
-                String currentInnerKey = innerKey;
-                System.out.println(mainKey + " ---- " + innerKey + " ---- " + innerMap.get(innerKey));
-            }
+    public static void main(String[] args) {
+        try {
+            int i = 12;
+            int j = 0;
+            int result = i/j;
+            System.out.println(result);
+        } catch (NumberFormatException ex){
+            System.out.println(ex.getMessage() + "!!!");
+        } catch (ArithmeticException ex){
+            System.out.println(ex.getMessage() + "...");
+        } catch (Exception ex){
+            System.out.println(ex.getMessage() + ",,,,,");
         }
+        System.out.println("test");
     }
 }
